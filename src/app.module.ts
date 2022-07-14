b0240f92b2e119c7eb3d9ec6892b2cb34f43ae6b
rafamagalhas <magalhas.rafa@gmail.com>
2022-07-13 21:35:23 -0300

    fix: routes in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { RouterModule } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CardModule } from './card/card.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [],
+  imports: [
+    CardModule,
+    HealthModule,
+    RouterModule.register([
+      {
+        path: 'api/v1',
+        children: [
+          {
+            path: 'health',
+            module: HealthModule,
+          },
+          {
+            path: 'cards',
+            module: CardModule,
+          },
+        ],
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
